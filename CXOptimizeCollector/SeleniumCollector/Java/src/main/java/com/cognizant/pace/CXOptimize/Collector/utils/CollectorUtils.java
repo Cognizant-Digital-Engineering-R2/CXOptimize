d3af6baf3ad3c82bfe69a2b22a200af9c4540ed2
senthilkumarchinnasamycognizant <Senthilkumar.Chinnasamy@cognizant.com>
2018-12-27 15:59:43 -0500

    Defect fix for manual resource clear

--- CXOptimizeCollector/SeleniumCollector/Java/src/main/java/com/cognizant/pace/CXOptimize/Collector/utils/CollectorUtils.java
+++ CXOptimizeCollector/SeleniumCollector/Java/src/main/java/com/cognizant/pace/CXOptimize/Collector/utils/CollectorUtils.java
@@ -279,15 +279,16 @@ public class CollectorUtils {
                     }
                     jsExe.executeScript(CollectorConstants.clearResourceTiming());
 
-                    if (CollectorConstants.getManualResourceTimeClear().equals("true") && !navType) {
+                    if (CollectorConstants.getManualResourceTimeClear().equals("true") && !navType)
+                    {
                         LOGGER.debug("CXOP - {} - Manual Resource Truncation is enabled", txnName);
                         LOGGER.debug("CXOP - {} - Size of resource before Manual Resource Truncation : {}", txnName, resourceDetails.size());
                         LOGGER.debug("CXOP - {} - Previous Hard Txn Start : {} , Script Start : {} ", txnName, CollectorConstants.getPrevTxnStartTime(),CollectorConstants.getScriptStartTime());
-                        long diff =  (CollectorConstants.getScriptStartTime() - CollectorConstants.getPrevTxnStartTime());
-                        LOGGER.debug("CXOP - {} - Time between first transaction and current transaction : {}", txnName, diff);
+                        //long diff =  (CollectorConstants.getScriptStartTime() - CollectorConstants.getPrevTxnStartTime());
+                        //LOGGER.debug("CXOP - {} - Time between first transaction and current transaction : {}", txnName, diff);
                         //remove all resources start time less than the difference
                         LOGGER.debug("CXOP - {} - Resource Before Truncation {}", resourceDetails.toString());
-                        resourceDetails.removeIf(s -> (CollectorConstants.getPrevTxnStartTime() + Long.parseLong(s.get("startTime").toString())) < diff);
+                        resourceDetails.removeIf(s -> (CollectorConstants.getPrevTxnStartTime() + Math.round(Double.parseDouble(s.get("startTime").toString()))) < CollectorConstants.getScriptStartTime());
                         LOGGER.debug("CXOP - {} - Resource After Truncation {}", resourceDetails.toString());
                         LOGGER.debug("CXOP - {} - Size of resource after Manual Resource Truncation : {}", txnName, resourceDetails.size());
                     }
@@ -312,7 +313,7 @@ public class CollectorUtils {
                     collectedData.put("visuallyComplete", ((long) navigationDetails.get("loadEventEnd") - (long) navigationDetails.get("navigationStart")));
                 }
 
-                if (navigationDetails == null && resourceDetails.size() <= 0) {
+                if (navigationDetails == null && filteredResources.size() <= 0) {
                     LOGGER.info("CXOP - {} - probably did not make a server request and hence will be ignored.", txnName);
                     return rtnValue;
                 }
